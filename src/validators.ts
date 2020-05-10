e9f57a7477e10528273008db400bcd81a9e49d71
Jorge Ferrero Linacero <jorge.ferreero.linacero@hp.com>
2020-05-10 12:28:16 +0200

    Refactor: Remove dead code

--- src/validators.ts
+++
@@ -1,7 +0,0 @@
-export const isPositiveInteger = (value: number): boolean => {
-    return value >= 0 && Number.isInteger(value);
-};
-
-export const isEvenNumber = (value: number): boolean => {
-    return value % 2 === 0;
-};
