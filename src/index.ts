7487f08b64cd8f2487ead03812943cb3d8952e44
Jorge Ferrero Linacero <jorge.ferreero.linacero@hp.com>
2020-05-10 12:35:27 +0200

    Update index.ts
    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 // import { createInterface } from 'readline';
 
-// import { Grid } from './grid';
-// import { parseCoordinates } from './parsers';
-// import Simulator from './simulator';
-
 // const input = createInterface({
 //     input: process.stdin,
 //     output: process.stdout
 // });
-
-// let simulator: Simulator;
 
 // function main(): void {
 //     input.question('Add mars grid dimensions', (coordinates: string): void => {
@@ -18,23 +12,15 @@
 //     });
 // }
 
-// const gridDimensionsFromConsole = (coordinates: string): void => {
+// const gridDimensionsFromConsole = (_: string): void => {
 //     try {
-//         const grid = parseCoordinates(coordinates);
-//         simulator = new Simulator(newGrid(grid));
+//         //
 //     } catch (error) {
 //         // tslint:disable-next-line:no-console
 //         console.error(error);
 //         return;
 //     }
 //     input.on('line', robotFromConsole);
-// };
-
-// const newGrid = (coordinates: number[]): Grid => {
-//     return {
-//         x: coordinates[0],
-//         y: coordinates[1]
-//     };
 // };
 
 // const robotFromConsole = (robot: string): void => {
