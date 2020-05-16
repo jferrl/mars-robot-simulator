0a33a9d5b3ddbd9f790748101b4377bad86d6e64
Jorge Ferrero Linacero <jorgeferrerolinacero@gmail.com>
2020-05-16 10:56:17 +0200

    Create rover-factory.test.ts
    - Create unit tests for rover factory

---
+++ test/rover-factory.test.ts
@@ -0,0 +1,21 @@
+import { Orientation } from '../src/position';
+
+import { aPoint, aState, roverFactory } from './helpers';
+
+describe('RoverFactory', (): void => {
+    describe('constructor', (): void => {
+        it('should create a defined instance', (): void => {
+            expect(roverFactory()).toBeDefined();
+        });
+    });
+
+    describe('createRobot', (): void => {
+        it('should throw an error if state is not defined', (): void => {
+            const state: any = undefined;
+            expect((): any => roverFactory().createRobot(state)).toThrow();
+        });
+        it('should create a defined robot instance', (): void => {
+            expect(roverFactory().createRobot(aState(aPoint(0, 0), Orientation.North))).toBeDefined();
+        });
+    });
+});
